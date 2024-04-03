1dc067393573e8536767c2e5c87bd755343d67dd
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-04-02 20:31:52 -0400

    Added PolyStr function

--- Tests/test_Util.py
+++ Tests/test_Util.py
@@ -7,12 +7,13 @@ from unittest import TestCase
 
 # system packages
 import numpy as np
+from numpy import isclose
 
 # libraries to be tested
 import Util
 
-class Test(TestCase):
-  def test_PolyValA(self):
+class TestPolyVal(TestCase):
+  def test_PolyVal_oa(self):
     coef = [1, 0, 0, 1]
 
     val = Util.PolyVal(coef, 0)
@@ -24,7 +25,7 @@ class Test(TestCase):
     val = Util.PolyVal(coef, -1)
     self.assertEqual(0, val)
 
-  def test_PolyValB(self):
+  def test_PolyVal_0b(self):
     coef = [-1, -2, -1]
 
     val = Util.PolyVal(coef, 0)
@@ -36,13 +37,30 @@ class Test(TestCase):
     val = Util.PolyVal(coef, -1)
     self.assertEqual(0, val)
 
-  def test_PolyValC(self):
+  def test_PolyVal_0c(self):
     coef = np.array([-1, -2, -1])
     x = np.array([0,1,-1])
     val = Util.PolyVal(coef, x)
     self.assertEqual([-1, -4, 0], val)
 
-  def test_PolyValD(self):
+  def test_PolyVal_0d(self):
     coef = [1, 0.2, 0.7, 1, -0.5]
     val = Util.PolyVal(coef, 2)
-    self.assertEqual(2.4, val)
+    self.assertTrue(isclose(4.2, val))
+    
+    
+class TestPolyStr(TestCase):
+  def test_PolyStr_0a(self):
+    coefs = [0, 1, 0, 1]
+    result = Util.PolyStr(coefs, 'z')
+    self.assertEqual('z^3 + z', result)
+
+  def test_PolyStr_0b(self):
+    coefs = [-1, 2, -3, 4]
+    result = Util.PolyStr(coefs, 'q')
+    self.assertEqual('4q^3 - 3q^2 + 2q - 1', result)
+
+  def test_PolyStr_0c(self):
+    coefs = [1, 0.2, 0.7, 1, -0.5]
+    result = Util.PolyStr(coefs, 'x')
+    self.assertEqual('-0.5x^4 + x^3 + 0.7x^2 + 0.2x + 1', result)
--- Util.py
+++ Util.py
@@ -3,6 +3,8 @@
 #
 
 import numpy as np
+from math import floor
+import random
 
 
 # Note on the order of the coefficients:  coef[i] is the coefficient of the term x^i.
@@ -22,3 +24,39 @@ def PolyVal(coef, xL):
   else:
     result = [PolyVal1(coef, x) for x in xL]
     return result
+
+def PolyStr(coefL, varStr):
+  ord = len(coefL) - 1
+
+  result = ''
+  for i in range(ord, -1, -1):
+    coef = coefL[i]
+
+    if (coef != 0):
+      if (i < ord):
+        if (0 < coef):
+          result += ' + '
+        elif (coef < 0):
+          result += ' - '
+
+      if (abs(coef) != 1) or (i == 0):
+        if (i == ord):
+          result += f'{coef}'
+        else:
+          result += f'{abs(coef)}'
+
+      if 1 < i:
+        result += f'{varStr}^{i}'
+      elif i == 1:
+        result += f'{varStr}'
+
+  return result
+
+def SetSeed(seed, dig=5):
+  if seed is None:
+    random.seed()
+    seed = random.randint(0, 10 ** dig - 1)
+
+  random.seed(seed)
+
+  return seed
